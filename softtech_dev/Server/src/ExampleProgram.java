a6972d6fa0f82265cc993e372b4c480a8f054af0
prasad <prasad4308@gmail.com>
2016-05-31 12:40:43 +0530

    soft

---
+++ softtech_dev/Server/src/ExampleProgram.java
@@ -0,0 +1,5 @@
+class ExampleProgram {
+  public static void main(String[] args){
+    System.out.println("Simple Java source code to demonstrate\ncommandline or manual compilation process");
+  }
+}
